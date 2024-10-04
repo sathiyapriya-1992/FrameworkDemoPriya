6a2a132d4820199eca09aaffc38b7febdf657c6a
sathiyapriya <sathiyapriya@Sathiyapriya>
2024-10-04 14:03:40 +0530

    Pushing framework

---
+++ ActualFramework/src/test/java/contactTests/ToCreateOrganization.java
@@ -0,0 +1,10 @@
+package contactTests;
+
+public class ToCreateOrganization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ActualFramework/src/test/java/organizationTests/ToCreateOrganization.java
@@ -0,0 +1,10 @@
+package organizationTests;
+
+public class ToCreateOrganization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
